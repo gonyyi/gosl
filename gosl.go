740c03750421ebb5202589e524de1734d102635f
Gon Yi <gonyyi@users.noreply.github.com>
2021-11-03 13:44:54 -0500

    add OnPanic(string)

--- gosl.go
+++ gosl.go
@@ -1,5 +1,5 @@
 // (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
-// Last Update: 11/1/2021
+// Last Update: 11/3/2021
 
 package gosl
 
@@ -50,4 +50,44 @@ func getBufpBuffer() *bufpBuffer {
 	return bufp.Get().(*bufpBuffer)
 }
 
+
+// OnPanic will take name and a function func(error),
+// if function f is given, it will use that function,
+// if not given, print the message using println (stdout)
+// Usage:
+//     func hello() (out string) {
+//         defer OnPanic("hello", func(m string) { out = m })
+//         panic("whatever")
+//     }
+func OnPanic(name string, f func(m string)) {
+	// This will only execute when recover() has something
+	if r := recover(); r != nil {
+		// Since `panic(interface{})` can be called with any value including error and string,
+		// convert it to a string and will save to `m`
+		var m string
+
+		// based on the type of message from `recover()`, get string out of it.
+		// if it was unexpected type, then set `m` with `<unknown>`.
+		switch v := r.(type) {
+		case error:
+			m = v.Error()
+		case string:
+			m = v
+		default:
+			m = "<unknown>"
+		}
+
+		// if function function `f` is given, use it.
+		// otherwise, write it to stdout using print.
+		if f != nil {
+			f(m)
+		} else {
+			// When no function is given, print it to screen
+			buf := make(Buf, 0, 1024)
+			buf = buf.WriteString(name).
+				WriteString(":Panic -> ").WriteString(m)
+			println(buf.String())
+		}
+	}
+}
 
