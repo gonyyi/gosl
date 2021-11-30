118b436c2ccc70b93091ead184ddb9e8d5bc2a93
Gon <29622355+gonyyi@users.noreply.github.com>
2021-11-30 10:24:36 -0600

    Update bytes_test.go

--- bytes_test.go
+++ bytes_test.go
@@ -1,5 +1,5 @@
 // (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
-// Last Update: 11/19/2021
+// Last Update: 11/30/2021
 
 package gosl_test
 
@@ -7,6 +7,36 @@ import (
 	"github.com/gonyyi/gosl"
 	"testing"
 )
+
+func TestNewBytesFilter(t *testing.T) {
+	buf := make(gosl.Buf, 0, 512)
+	b := []byte("0123456789")
+
+	f := gosl.NewBytesFilter(true, b) // only allow numbers
+	buf = f([]byte("abc123"))
+
+	gosl.Test(t, "123", buf.String())
+
+	f = gosl.NewBytesFilter(false, b) // disallow numbers
+	buf = f([]byte("abc123"))
+
+	gosl.Test(t, "abc", buf.String())
+}
+
+func TestBytesInsert(t *testing.T) {
+	buf := make(gosl.Buf, 0, 512)
+	buf = buf.WriteString("GON123")
+	buf = gosl.BytesInsert(buf, 3, []byte(" says "))
+	gosl.Test(t, "GON says 123", buf.String())
+
+	buf = buf.Reset().WriteString("GON123")
+	buf = gosl.BytesInsert(buf, 6, []byte(" here"))
+	gosl.Test(t, "GON123 here", buf.String())
+
+	buf = buf.Reset().WriteString("GON123")
+	buf = gosl.BytesInsert(buf, 7, []byte("here"))
+	gosl.Test(t, "GON123here", buf.String())
+}
 
 func TestBytesToUpper(t *testing.T) {
 	tmp := []byte("abcdefghijklmnopqrstuvwxyz-ABCDEFGHIJKLMNOPQRSTUVWXYZ-0123456789-GonYiIsHere")
@@ -21,7 +51,7 @@ func BenchmarkBytesToUpper(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			buf = gosl.BytesToUpper(buf)
 		}
-		//println(string(Buffer))
+		// println(string(Buffer))
 	})
 }
 
@@ -38,6 +68,6 @@ func BenchmarkBytesToLower(b *testing.B) {
 		for i := 0; i < b.N; i++ {
 			buf = gosl.BytesToLower(buf)
 		}
-		//println(string(Buffer))
+		// println(string(Buffer))
 	})
 }
