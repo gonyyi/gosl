08b69eef7b7c2358d5905c97c2ee428bc5690d22
Gon <29622355+gonyyi@users.noreply.github.com>
2021-11-30 10:26:08 -0600

    IntsJoin no longer use default delimiter

--- int.go
+++ int.go
@@ -1,5 +1,5 @@
 // (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
-// Last Update: 11/19/2021
+// Last Update: 11/30/2021
 
 package gosl
 
@@ -9,9 +9,7 @@ func IntsJoin(dst []byte, p []int, delim ...byte) []byte {
 	buf := make(Buf, 0, 4096)
 	for i, v := range p {
 		if i != 0 {
-			if delim == nil {
-				buf = buf.WriteBytes(',')
-			} else {
+			if delim != nil {
 				buf = buf.WriteBytes(delim...)
 			}
 		}
