c708626c084edf597c9c1117a18f855815d442d3
Gon <29622355+gonyyi@users.noreply.github.com>
2021-12-06 12:38:50 -0600

    Update conv.go

--- conv.go
+++ conv.go
@@ -1,5 +1,5 @@
 // (c) Gon Y. Yi 2021 <https://gonyyi.com/copyright>
-// Last Update: 11/19/2021
+// Last Update: 12/06/2021
 
 package gosl
 
@@ -98,4 +98,116 @@ func ToLower(s string) string {
 	buf := make([]byte, 0, 128)
 	buf = BytesToLower(append(buf, s...))
 	return string(buf)
+}
+
+// BytesToHex will take byte (or bytes) and append it to dst
+// Eg. BytesToHex(nil, []byte("Hello Gon")) // => 48656c6c6f20476f6e
+func BytesToHex(dst []byte, b []byte) []byte {
+	for _, c := range b {
+		d, r := int(c)/16, int(c)%16
+		if d < 10 {
+			d = '0' + d
+		} else {
+			d = 'a' + (d - 10)
+		}
+		if r < 10 {
+			r = '0' + r
+		} else {
+			r = 'a' + (r - 10)
+		}
+		dst = append(dst, byte(d), byte(r))
+	}
+	return dst
+}
+
+// StringToHex will take string and append it to dst
+// Eg. StringToHex(nil, "Hello Gon") // => 48656c6c6f20476f6e
+func StringToHex(dst []byte, b string) []byte {
+	for _, c := range b {
+		d, r := int(c)/16, int(c)%16
+		if d < 10 {
+			d = '0' + d
+		} else {
+			d = 'a' + (d - 10)
+		}
+		if r < 10 {
+			r = '0' + r
+		} else {
+			r = 'a' + (r - 10)
+		}
+		dst = append(dst, byte(d), byte(r))
+	}
+	return dst
+}
+
+// HexToBytes will convert hex string and append it to byte
+// Note that this is code is completely identical to HexStringToBytes.
+// Eg. HexToBytes(nil, "48656c6c6f20476f6e") // => Hello Gon
+func HexToBytes(dst []byte, hex []byte) (out []byte, ok bool) {
+	// Validate the length
+	if len(hex)%2 != 0 {
+		return dst, false
+	}
+	// Validate if only expected are there
+	for i := 0; i < len(hex); i++ {
+		if ('0' > hex[i] || '9' < hex[i]) && ('a' > hex[i] || 'z' < hex[i]) {
+			return dst, false
+		}
+	}
+	// For each pair (as 2 hex code will be 1 byte)
+	for i := 0; i < len(hex); i += 2 {
+		var cidx int // character index
+		// First char of the hex code
+		if '0' <= hex[i] && hex[i] <= '9' {
+			// for the first char, it will need to multiply by 16
+			cidx += int(hex[i]-'0') * 16
+		} else if 'a' <= hex[i] && hex[i] <= 'z' {
+			// since the order is 0-9 then a-f, a-f will need to be +10.
+			cidx += (int(hex[i]-'a') + 10) * 16
+		}
+		// Second char of the hex code
+		if '0' <= hex[i+1] && hex[i+1] <= '9' {
+			cidx += int(hex[i+1] - '0')
+		} else if 'a' <= hex[i+1] && hex[i+1] <= 'z' {
+			cidx += int(hex[i+1]-'a') + 10
+		}
+
+		dst = append(dst, byte(cidx))
+	}
+
+	return dst, true
+}
+
+// HexStringToBytes will convert hex string and append it to byte
+// Note that this is code is completely identical to HexToBytes.
+// Eg. HexStringToBytes(nil, "48656c6c6f20476f6e") // => Hello Gon
+func HexStringToBytes(dst []byte, hex string) (out []byte, ok bool) {
+	if len(hex)%2 != 0 {
+		return dst, false
+	}
+
+	for i := 0; i < len(hex); i++ {
+		if ('0' > hex[i] || '9' < hex[i]) && ('a' > hex[i] || 'z' < hex[i]) {
+			return dst, false
+		}
+	}
+
+	for i := 0; i < len(hex); i += 2 {
+		var cidx int // character index
+		if '0' <= hex[i] && hex[i] <= '9' {
+			cidx += int(hex[i]-'0') * 16
+		} else if 'a' <= hex[i] && hex[i] <= 'z' {
+			cidx += (int(hex[i]-'a') + 10) * 16
+		}
+
+		if '0' <= hex[i+1] && hex[i+1] <= '9' {
+			cidx += int(hex[i+1] - '0')
+		} else if 'a' <= hex[i+1] && hex[i+1] <= 'z' {
+			cidx += int(hex[i+1]-'a') + 10
+		}
+
+		dst = append(dst, byte(cidx))
+	}
+
+	return dst, true
 }
