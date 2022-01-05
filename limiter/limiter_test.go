127fee94033da7930c0bfbca29a68a71296a323c
Gon <29622355+gonyyi@users.noreply.github.com>
2022-01-04 18:59:04 -0600

    Create limiter_test.go

---
+++ limiter/limiter_test.go
@@ -0,0 +1,43 @@
+// (c) Gon Y. Yi 2021-2022 <https://gonyyi.com/copyright>
+// Last Update: 01/04/2022
+
+package limiter_test
+
+import (
+  "github.com/gonyyi/gosl/limiter"
+	"testing"
+)
+
+func TestLimiter(t *testing.T) {
+	// Create a limit with 10 jobs at a time
+	// > limit := limiter.Limiter{}
+	// > limit.Init(12)
+	// Or simply,
+	// > limit := NewLimiter(10)
+
+	limit := limiter.Limiter{}
+  
+	run := func(z int) {
+		// println("\nSTEP ",z," -------------")
+		limit.Init(5)
+
+		for i := 0; i < 10000; i++ {
+			limit.Ready()
+			go func() {
+				limit.Done()
+			}()
+		}
+
+		limit.Wait()
+		limit.Close()
+		// println("\tDone: ", limit.Started(), "/", limit.Finished(), "/", limit.Running())
+		if limit.Started() != 10000 || limit.Finished() != 10000 || limit.running != 0 {
+			t.Errorf("Limiter Test %d - Started: %d, Finished: %d, Running: %d", z, limit.Started(), limit.Finished(), limit.Running())
+			t.Fail()
+		}
+	}
+
+	for i:=0; i<10; i++ {
+		run(i)
+	}
+}
