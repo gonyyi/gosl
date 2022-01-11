8cd2f4737c91b10cd87d075653c1dca075c930eb
Gon Yi <29622355+gonyyi@users.noreply.github.com>
2022-01-10 20:42:44 -0600

    modify cover.sh to write _cover.html, _cover.out instead of cover.html and cover.out

--- cover.sh
+++ cover.sh
@@ -1,3 +1,4 @@
 #!/bin/sh
-go test -v -coverprofile cover.out .
-go tool cover -html=cover.out -o cover.html
+go test -v -coverprofile _cover.out .
+go tool cover -html=_cover.out -o _cover.html
+open _cover.html
