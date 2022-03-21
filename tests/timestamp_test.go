d83e0dca252ea40edfb715080831712e116aa59f
Gon <29622355+gonyyi@users.noreply.github.com>
2022-03-21 13:52:33 -0500

    Update and rename ts_test.go to timestamp_test.go

--- tests/timestamp_test.go
+++ tests/timestamp_test.go
@@ -9,16 +9,16 @@ import (
 	"testing"
 )
 
-func TestTS(t *testing.T) {
-	var tsz = gosl.TS(0)
+func TestTimestamp(t *testing.T) {
+	var tsz = gosl.Timestamp(0)
 
-	eqTS := func(id int, exp, act gosl.TS) {
+	eqTS := func(id int, exp, act gosl.Timestamp) {
 		if exp != act {
 			t.Errorf("[%04d] Failed: Exp=%d, Act=%d\n", id, exp, act)
 		}
 	}
 	eqI64 := func(id int, exp, act int64) {
-		eqTS(id, gosl.TS(exp), gosl.TS(act))
+		eqTS(id, gosl.Timestamp(exp), gosl.Timestamp(act))
 	}
 	eqStr := func(id int, exp, act string) {
 		if exp != act {
@@ -38,8 +38,8 @@ func TestTS(t *testing.T) {
 	})
 
 	t.Run("Valid()", func(t *testing.T) {
-		eqBool(1010, true, gosl.TS(20060102150405123).IsValid())
-		eqBool(1010, false, gosl.TS(2006010215040512).IsValid())
+		eqBool(1010, true, gosl.Timestamp(20060102150405123).IsValid())
+		eqBool(1010, false, gosl.Timestamp(2006010215040512).IsValid())
 	})
 
 	t.Run("SetDate(),SetTime()", func(t *testing.T) {
@@ -71,15 +71,15 @@ func TestTS(t *testing.T) {
 		{
 			out := tmp.Byte()
 			res := binary.LittleEndian.Uint64(out[:]) // parse [8]byte to int
-			eqTS(100, tmp, gosl.TS(res))
+			eqTS(100, tmp, gosl.Timestamp(res))
 		}
-		// byte using binary, then parse it with TS
+		// byte using binary, then parse it with Timestamp
 		{
 			out := make([]byte, 8)
 			binary.LittleEndian.PutUint64(out, uint64(tmp)) // create uint64 to byte
-			var out2 [8]byte                                // TS takes array, but binary library returns slice
+			var out2 [8]byte                                // Timestamp takes array, but binary library returns slice
 			copy(out2[:], out[:])
-			eqTS(200, 20060102150405123, gosl.TS(0).ParseByte(out2))
+			eqTS(200, 20060102150405123, gosl.Timestamp(0).ParseByte(out2))
 		}
 	})
 
@@ -92,28 +92,28 @@ func TestTS(t *testing.T) {
 		eqStr(1060, "20060102150405123", tsz.Parse("2006-01-02-15-04-05-123", 0).String())
 	})
 
-	t.Run("Format()", func(t *testing.T) {
-		eqStr(1010, "2006/01/02 15:04:05.000", tsz.Parse("20060102150405", 0).Format(0))
-		eqStr(1020, "2006/01/02 15:04:05.123", tsz.Parse("20060102150405123", 0).Format(0))
-		eqStr(1030, "2006/01/02 15:04:05.000", tsz.Parse("2006/01/02 15:04:05", 0).Format(0))
-		eqStr(1040, "2006/01/02 15:04:05.123", tsz.Parse("2006/01/02 15:04:05.123", 0).Format(0))
-		eqStr(1050, "2006/01/02 15:04:05.000", tsz.Parse("2006-01-02 15-04-05", 0).Format(0))
-		eqStr(1060, "2006/01/02 15:04:05.123", tsz.Parse("2006-01-02-15-04-05-123", 0).Format(0))
+	t.Run("Formats()", func(t *testing.T) {
+		eqStr(1010, "2006/01/02 15:04:05.000", tsz.Parse("20060102150405", 0).Formats(0))
+		eqStr(1020, "2006/01/02 15:04:05.123", tsz.Parse("20060102150405123", 0).Formats(0))
+		eqStr(1030, "2006/01/02 15:04:05.000", tsz.Parse("2006/01/02 15:04:05", 0).Formats(0))
+		eqStr(1040, "2006/01/02 15:04:05.123", tsz.Parse("2006/01/02 15:04:05.123", 0).Formats(0))
+		eqStr(1050, "2006/01/02 15:04:05.000", tsz.Parse("2006-01-02 15-04-05", 0).Formats(0))
+		eqStr(1060, "2006/01/02 15:04:05.123", tsz.Parse("2006-01-02-15-04-05-123", 0).Formats(0))
 
-		eqStr(1020, "2016/01/02 15:04:05.123", tsz.Parse("20160102150405123", 0).Format(gosl.TSFmtDefault))
+		eqStr(1020, "2016/01/02 15:04:05.123", tsz.Parse("20160102150405123", 0).Formats(gosl.TDefault))
 
-		eqStr(1020, "2016/01/02", tsz.Parse("20160102150405125", 0).Format(gosl.TSFmtYearDate))
-		eqStr(1020, "2016/01/02 15:04", tsz.Parse("20160102150405125", 0).Format(gosl.TSFmtYearDate|gosl.TSFmtTime))
-		eqStr(1020, "2016/01/02 15:04:05", tsz.Parse("20160102150405125", 0).Format(gosl.TSFmtYearDate|gosl.TSFmtTimeSec))
-		eqStr(1020, "2016/01/02 15:04:05.123", tsz.Parse("20160102150405123", 0).Format(gosl.TSFmtYearDate|gosl.TSFmtTimeMS))
+		eqStr(1020, "2016/01/02", tsz.Parse("20160102150405125", 0).Formats(gosl.TYrMoDay))
+		eqStr(1020, "2016/01/02 15:04", tsz.Parse("20160102150405125", 0).Formats(gosl.TYrMoDay|gosl.THrMin))
+		eqStr(1020, "2016/01/02 15:04:05", tsz.Parse("20160102150405125", 0).Formats(gosl.TYrMoDay|gosl.THrMinSec))
+		eqStr(1020, "2016/01/02 15:04:05.123", tsz.Parse("20160102150405123", 0).Formats(gosl.TYrMoDay|gosl.THrMinSecMs))
 
-		eqStr(1020, "01/02", tsz.Parse("20160102150405126", 0).Format(gosl.TSFmtDate))
-		eqStr(1020, "01/02 15:04", tsz.Parse("20160102150405126", 0).Format(gosl.TSFmtDate|gosl.TSFmtTime))
-		eqStr(1020, "01/02 15:04:05", tsz.Parse("20160102150405126", 0).Format(gosl.TSFmtDate|gosl.TSFmtTimeSec))
-		eqStr(1020, "01/02 15:04:05.126", tsz.Parse("20160102150405126", 0).Format(gosl.TSFmtDate|gosl.TSFmtTimeMS))
+		eqStr(1020, "01/02", tsz.Parse("20160102150405126", 0).Formats(gosl.TMoDay))
+		eqStr(1020, "01/02 15:04", tsz.Parse("20160102150405126", 0).Formats(gosl.TMoDay|gosl.THrMin))
+		eqStr(1020, "01/02 15:04:05", tsz.Parse("20160102150405126", 0).Formats(gosl.TMoDay|gosl.THrMinSec))
+		eqStr(1020, "01/02 15:04:05.126", tsz.Parse("20160102150405126", 0).Formats(gosl.TMoDay|gosl.THrMinSecMs))
 
-		eqStr(1020, "15:04", tsz.Parse("20160102150405124", 0).Format(gosl.TSFmtTime))
-		eqStr(1020, "15:04:05", tsz.Parse("20160102150405124", 0).Format(gosl.TSFmtTimeSec))
-		eqStr(1020, "15:04:05.124", tsz.Parse("20160102150405124", 0).Format(gosl.TSFmtTimeMS))
+		eqStr(1020, "15:04", tsz.Parse("20160102150405124", 0).Formats(gosl.THrMin))
+		eqStr(1020, "15:04:05", tsz.Parse("20160102150405124", 0).Formats(gosl.THrMinSec))
+		eqStr(1020, "15:04:05.124", tsz.Parse("20160102150405124", 0).Formats(gosl.THrMinSecMs))
 	})
 }
